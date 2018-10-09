ae090c879bbcc78aad10402e26dfa34f32b2fbf9
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-10-09 08:58:39 -0700

    Agregar archivos de proyecto.

---
+++ ExamenSegundoParcial/InformacionPersonal.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamenSegundoParcial
+{
+    /// <summary>
+    /// Lógica de interacción para InformacionPersonal.xaml
+    /// </summary>
+    public partial class InformacionPersonal : UserControl
+    {
+        public InformacionPersonal()
+        {
+            InitializeComponent();
+        }
+
+    
+    }
+}
+
+/* if(NombreIP.Text == "")
+            {
+                MessageBox.Show("Es necesario llenar todos los campos");
+            }
+            if (ApellidoIP.Text == "")
+            {
+                MessageBox.Show("Es necesario llenar todos los campos");
+            }
+            if (Apellido2IP.Text == "")
+            {
+                MessageBox.Show("Es necesario llenar todos los campos");
+            }
+            if (EdadIP.Text == "")
+            {
+                MessageBox.Show("Es necesario llenar todos los campos");
+            }
+            */
---
+++ ExamenSegundoParcial/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamenSegundoParcial
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSiguiente_Click(object sender, RoutedEventArgs e)
+        {
+            if (informacionPersonal.NombreIP.Text == "" || informacionPersonal.ApellidoIP.Text == "" || informacionPersonal.Apellido2IP.Text == "" || informacionPersonal.EdadIP.Text == "")
+            {
+                informacionPersonal.error.Visibility = Visibility.Visible;
+                
+            } else
+            {
+                contacto.Visibility = Visibility.Visible;
+            }
+
+            if (contacto.correoC.Text == "" || contacto.telefonoC.Text == "" || contacto.celularC.Text == "")
+            {
+                contacto.error2.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                informacionPago.Visibility = Visibility.Visible;
+                btnFinalizar.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void btnFinalizar_Click(object sender, RoutedEventArgs e)
+        {
+
+            btnSiguiente.Visibility = Visibility.Hidden;
+            btnFinalizar.Visibility = Visibility.Visible;
+            
+            if (informacionPago.numeroTarjeta.MaxLength > 16 || informacionPago.numeroTarjeta.MaxLength < 16)
+            {
+                informacionPago.error3.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
